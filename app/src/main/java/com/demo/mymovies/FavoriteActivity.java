970a346e2ba733ef8568328ecde257ba1d22d329
Nishpo <nishpo@yandex.ru>
2022-03-30 13:19:42 +0300

    Initial commit

---
+++ app/src/main/java/com/demo/mymovies/FavoriteActivity.java
@@ -0,0 +1,86 @@
+package com.demo.mymovies;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.demo.mymovies.adapters.MovieAdapter;
+import com.demo.mymovies.data.FavouriteMovie;
+import com.demo.mymovies.data.MainViewModel;
+import com.demo.mymovies.data.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.itemMain:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.itemFavourite:
+                Intent intentToFavourite = new Intent(this, FavoriteActivity.class);
+                startActivity(intentToFavourite);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private RecyclerView recyclerViewFavouriteMovies;
+    private MovieAdapter adapter;
+    private MainViewModel viewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+
+        adapter = new MovieAdapter();
+        recyclerViewFavouriteMovies = findViewById(R.id.recyclerViewFavouriteMovies);
+        recyclerViewFavouriteMovies.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerViewFavouriteMovies.setAdapter(adapter);
+        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(MainViewModel.class);
+        LiveData<List<FavouriteMovie>> favouriteMovies = viewModel.getFavouriteMovies();
+        favouriteMovies.observe(this, new Observer<List<FavouriteMovie>>() {
+            @Override
+            public void onChanged(List<FavouriteMovie> favouriteMovies) {
+                List<Movie> movies = new ArrayList<>();
+                if (favouriteMovies != null) {
+                    movies.addAll(favouriteMovies);
+                    adapter.setMovies(movies);
+                }
+            }
+        });
+
+        adapter.setOnPosterClickListener(new MovieAdapter.OnPosterClickListener() {
+            @Override
+            public void onPosterClick(int position) {
+                Movie movie = adapter.getMovies().get(position);
+                Intent intent = new Intent(FavoriteActivity.this, DetailActivity.class);
+                intent.putExtra("id", movie.getId());
+                startActivity(intent);
+            }
+        });
+    }
+}
